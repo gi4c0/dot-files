ac669a4915546998c0575a83d629c94b6d0309d7
alexpan <p.aleksey@do-it.co>
2019-03-11 19:48:07 +0200

    Add neotree

--- emacs/init.el
+++ emacs/init.el
@@ -32,10 +32,10 @@
  '(initial-frame-alist (quote ((fullscreen . maximized))))
  '(package-selected-packages
    (quote
-    (projectile evil-magit magit rainbow-mode rainbow avy smex ido-vertical-mode evil-surround org-bullets beacon evil spacemacs-theme which-key use-package))))
+    (neotree projectile evil-magit magit rainbow-mode rainbow avy smex ido-vertical-mode evil-surround org-bullets beacon evil spacemacs-theme which-key use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "#292b2e" :foreground "#b2b2b2" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 130 :width normal :foundry "nil" :family "Menlo")))))
+ '(default ((t (:inherit nil :stipple nil :background "#292b2e" :foreground "#b2b2b2" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 140 :width normal :foundry "nil" :family "Menlo")))))
