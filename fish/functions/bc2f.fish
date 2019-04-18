024254b4e0a440f906751dec605e8769b3da4411
gi4c0 <lordpan6909@gmail.com>
2019-04-18 22:02:20 +0300

    Add check for rabbitmq service to bc2f script

--- fish/functions/bc2f.fish
+++ fish/functions/bc2f.fish
@@ -1,4 +1,14 @@
+# Script for startup all the services required for work
+
 function bc2f
+  # Check if rabbitmq is already running, if not -> stat it
+  if ! systemctl is-active --quiet rabbitmq.service
+      echo "RabbitMQ is inactive. Starting up..."
+      sudo systemctl start rabbitmq.service
+  end
+
+  # Set current dir to var to be able to return ther after script is
+  # finished
   set current_dir $PWD
   pm2 delete all
 
