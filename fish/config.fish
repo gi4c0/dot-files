652cb6c3d77d9f355c4dc874baf08154ffac3a14
gi4c0 <lordpan6909@gmail.com>
2019-05-15 22:41:15 +0300

    Make default editor - nvim

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,7 @@
 
 # fnm
 set PATH $HOME/.fnm $HOME/bin $PATH
-set -gx EDITOR vim
+set -gx EDITOR nvim
 set -gx PASSWORD_STORE_GENERATED_LENGTH 20
 set -gx BROWSER (which google-chrome-stable)
 fnm env --multi | source
