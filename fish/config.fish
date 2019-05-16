8e8d55acd1295a94d8f01ec97d48660176086388
gi4c0 <lordpan6909@gmail.com>
2019-05-16 08:35:16 +0300

    [ranger] Add rc.conf

--- fish/config.fish
+++ fish/config.fish
@@ -3,5 +3,9 @@
 set PATH $HOME/.fnm $HOME/bin $PATH
 set -gx EDITOR nvim
 set -gx PASSWORD_STORE_GENERATED_LENGTH 20
+
+# Use custom config file to avoid loading it twice
+set -gx RANGER_LOAD_DEFAULT_RC 'FALSE'
+
 set -gx BROWSER (which google-chrome-stable)
 fnm env --multi | source
