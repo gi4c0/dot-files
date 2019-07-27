9cfd680932bb39b7ad2252ffc7bff9ee9e7b549e
alexpan <p.aleksey@do-it.co>
2019-07-27 16:49:16 +0300

    Add calculator to shortcut

--- fish/config.fish
+++ fish/config.fish
@@ -1,11 +1,9 @@
-
 # fnm
-set PATH $HOME/.fnm $HOME/bin /usr/local/go/bin $PATH/go/bin $PATH
+set PATH /home/alex/.fnm $PATH
+fnm env --multi | source
+
 set -gx EDITOR nvim
 set -gx PASSWORD_STORE_GENERATED_LENGTH 20
 
 # Use custom config file to avoid loading it twice
 set -gx RANGER_LOAD_DEFAULT_RC 'FALSE'
-
-set -gx BROWSER (which google-chrome-stable)
-fnm env --multi | source
