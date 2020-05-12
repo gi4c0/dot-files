78141eb22947aaae7dda27d9680364133abb084e
alexpan <lordpan6909@gmail.com>
2020-05-12 19:03:49 +0300

    Change server to a prod one for ssh

--- fish/functions/sshprod.fish
+++ fish/functions/sshprod.fish
@@ -1,7 +1,7 @@
 function sshprod
   if count $argv > /dev/null
-    ssh -L $argv[1]:localhost:$argv[1] denroot@denprdu1.eastus.cloudapp.azure.com
+      ssh -L $argv[1]:localhost:$argv[1] prodweb01@tcwprodweb01.eastus.cloudapp.azure.com
   else
-    ssh denroot@denprdu1.eastus.cloudapp.azure.com
+      ssh prodweb01@tcwprodweb01.eastus.cloudapp.azure.com
   end
 end
