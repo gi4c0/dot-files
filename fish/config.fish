3ce7e7334972c383a13efe4a503fc995469b1379
alexpan <lordpan6909@gmail.com>
2020-06-11 00:25:03 +0300

    Add fish functions for node (nvm lazy loading)

--- fish/config.fish
+++ fish/config.fish
@@ -1,9 +1,6 @@
 # fnm
-set PATH /home/alex/.fnm $PATH
-fnm env --multi | source
-
 set -gx EDITOR nvim
 set -gx PASSWORD_STORE_GENERATED_LENGTH 20
 
-# Use custom config file to avoid loading it twice
-set -gx RANGER_LOAD_DEFAULT_RC 'FALSE'
+set -gx DENO_INSTALL "/home/alex/.deno"
+set -gx PATH "$DENO_INSTALL/bin:$PATH"
