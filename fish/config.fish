2e687dcc573772964b28d3364be96f6daa008160
alexpan <lordpan6909@gmail.com>
2020-09-02 13:08:03 +0300

    Add path for GO

--- fish/config.fish
+++ fish/config.fish
@@ -3,4 +3,8 @@ set -gx EDITOR nvim
 set -gx PASSWORD_STORE_GENERATED_LENGTH 20
 
 set -gx DENO_INSTALL "/home/alex/.deno"
-set -gx PATH "$DENO_INSTALL/bin:$PATH"
+
+set -gx GOROOT "/usr/local/go"
+set -gx GOPATH "$HOME/go"
+
+set -gx PATH "$GOPATH/bin:$GOROOT/bin:$DENO_INSTALL/bin:$PATH"
