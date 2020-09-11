95204e7ecc012f1adaf35af4ea37dd195e85bed7
alexpan <lordpan6909@gmail.com>
2020-09-11 22:07:03 +0300

    Change font to support icons

--- fish/functions/sshprod.fish
+++
@@ -1,7 +0,0 @@
-function sshprod
-  if count $argv > /dev/null
-      ssh -L $argv[1]:localhost:$argv[1] prodweb01@tcwprodweb01.eastus.cloudapp.azure.com
-  else
-      ssh prodweb01@tcwprodweb01.eastus.cloudapp.azure.com
-  end
-end
--- fish/functions/sshschedular.fish
+++
@@ -1,4 +0,0 @@
-function sshschedular
-    set address prodscheduler01@tcwscheduler01prod.eastus.cloudapp.azure.com
-    ssh -L 9189:192.168.89.21:9189 $address
-end
