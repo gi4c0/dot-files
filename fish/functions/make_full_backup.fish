8cf168571248e7645347a01e3b21db608991f6f3
alexpan <lordpan6909@gmail.com>
2020-09-11 22:09:07 +0300

    Add fish config files

---
+++ fish/functions/make_full_backup.fish
@@ -0,0 +1,6 @@
+function make_full_backup -d "Run backup of all system to '/media/alex/Backup/regolith_v1.4_ubuntu_20_04/'"
+  sudo rsync -aAXv --delete --exclude="/dev/*" --exclude="/proc/*" \
+  --exclude="/sys/*" --exclude="/tmp/*" --exclude="/run/*" --exclude="/mnt/*" \
+  --exclude="/media/*" --exclude="swapfile" --exclude="lost+found" \
+  / /media/alex/Backup/regolith_v1.4_ubuntu_20_04/
+end
