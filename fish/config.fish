fb85e86be8e7b854c4d88b44d5b5d2d7e3ddfbc6
gi4c0 <panchenko.aleksey.a@gmail.com>
2020-12-01 20:04:21 +0200

    Add colors to i3

--- fish/config.fish
+++ fish/config.fish
@@ -2,9 +2,14 @@
 set -gx EDITOR nvim
 set -gx PASSWORD_STORE_GENERATED_LENGTH 20
 
-set -gx DENO_INSTALL "/home/alex/.deno"
-
 set -gx GOROOT "/usr/local/go"
 set -gx GOPATH "$HOME/go"
 
 set -gx PATH "$GOPATH/bin:$GOROOT/bin:$DENO_INSTALL/bin:$HOME/bin:$PATH"
+
+# start X at login
+if status --is-login
+    if test -z "$DISPLAY" -a $XDG_VTNR -eq 1
+        exec startx /home/alex/.dot-files/config/xinitrc -- -keeptty
+    end
+end
