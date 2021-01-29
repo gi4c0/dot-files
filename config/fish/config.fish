cb32c614c81608f4003050ace807f613d8f84511
alexpan <lordpan6909@gmail.com>
2021-01-29 09:58:24 +0200

    Add say configuration

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -3,19 +3,12 @@ set -gx EDITOR nvim
 
 # UNIX Pass: Defualt password length
 set -gx PASSWORD_STORE_GENERATED_LENGTH 20
+set -gx MOZ_ENABLE_WAYLAND 1
+set -gx QT_QPA_PLATFORM wayland
 
 # Required for golang
 # set -gx GOROOT "/usr/local/go"
 # set -gx GOPATH "$HOME/go"
-
-set -gx PATH "$HOME/.bin:/var/lib/snapd/snap/bin:/snap/bin:$PATH"
-
-# start X at login
-if status --is-login
-    if test -z "$DISPLAY" -a $XDG_VTNR -eq 1
-        exec startx /home/alex/.dot-files/config/xinitrc -- -keeptty
-    end
-end
 
 # Add <Ctrl-r> key binding from fzf
 fzf_key_bindings
--- config/nvim/themes/one.vim
+++ config/nvim/themes/one.vim
@@ -2,3 +2,6 @@ let g:airline_theme = 'one'
 let g:one_allow_italics = 1
 
 colorscheme one
+
+hi! Normal ctermbg=NONE guibg=NONE
+hi! NonText ctermbg=NONE guibg=NONE guifg=NONE ctermfg=NONE
