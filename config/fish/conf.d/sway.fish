999e02bffd811840de8298711b8df5fa9c2687f5
alexpan <panchenko.aleksey.a@gmail.com>
2021-02-07 16:17:39 +0200

    Move env variables to fish/conf.d

--- config/fish/conf.d/sway.fish
+++ config/fish/conf.d/sway.fish
@@ -1,5 +1,14 @@
 # If running from tty1 start sway
 set TTY1 (tty)
 if test -z "$DISPLAY"; and test $TTY1 = "/dev/tty1"
+  set -gx GTK_THEME Arc-Dark
+  set -gx EDITOR nvim
+
+  set -gx PASSWORD_STORE_GENERATED_LENGTH 20
+  set -gx MOZ_ENABLE_WAYLAND 1
+  set -gx QT_QPA_PLATFORM wayland
+
+  # output clip selection to stdout
+  set -gx CM_OUTPUT_CLIP 1
   exec sway
 end
--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,14 +1,2 @@
-# fnm
-set -gx EDITOR nvim
-
-# UNIX Pass: Defualt password length
-set -gx PASSWORD_STORE_GENERATED_LENGTH 20
-set -gx MOZ_ENABLE_WAYLAND 1
-set -gx QT_QPA_PLATFORM wayland
-
-# Required for golang
-# set -gx GOROOT "/usr/local/go"
-# set -gx GOPATH "$HOME/go"
-
 # Add <Ctrl-r> key binding from fzf
 fzf_key_bindings
