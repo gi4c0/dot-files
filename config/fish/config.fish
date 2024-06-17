5933952f527f6ccf6f779f314ed9c11797bca1ea
 Aleksey Panchenko <panchenko.aleksey.a@gmail.com>
2024-06-17 10:09:09 +0300

    Update fish config

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -19,6 +19,8 @@ set -gx PATH "$HOME/.bin:/opt/homebrew/opt/node@18/bin:/Users/alekseypanchenko/b
 abbr --add wb cd /Users/alekseypanchenko/projects/crysberry/w-backend
 abbr --add cry cd /Users/alekseypanchenko/projects/crysberry
 abbr --add ct cargo nextest run
-abbr --add ctn TRACE=1 cargo nextest run --no-capture --
+abbr --add ctl TRACE=1 cargo nextest run --no-capture --
+abbr --add k kubectl
+abbr --add mk minikube
 
 zoxide init --cmd cd fish | source
