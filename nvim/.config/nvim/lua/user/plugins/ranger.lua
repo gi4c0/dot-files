cf847faec533beee25ebf6bf510aa601c905f1a5
Oleksii Panchenko <panchenko.o@brainrocket.com>
2026-05-15 11:06:41 +0100

    Update ranger

--- nvim/.config/nvim/lua/user/plugins/ranger.lua
+++ nvim/.config/nvim/lua/user/plugins/ranger.lua
@@ -1,8 +1,8 @@
 return {
     'kevinhwang91/rnvimr',
-    setup = function()
+    lazy = false,
+    init = function()
         vim.g.rnvimr_enable_ex = true
-        vim.g.rnvimr_enable_picker = true
         vim.g.rnvimr_enable_picker = true
         vim.g.rnvimr_hide_gitignore = false
 
@@ -11,20 +11,21 @@ return {
             ['<C-v>'] = 'NvimEdit vsplit',
             ['<C-s>'] = 'NvimEdit split'
         }
+        vim.g.python3_host_prog = vim.fn.expand('~/.local/pipx/venvs/pynvim/bin/python')
 
-        vim.cmd [[
-            " Customize the initial layout
-            let g:rnvimr_layout = {
-                \ 'relative': 'editor',
-                \ 'width': float2nr(round(0.8 * &columns)),
-                \ 'height': float2nr(round(0.8 * &lines)),
-                \ 'col': float2nr(round(0.15 * &columns)),
-                \ 'row': float2nr(round(0.15 * &lines)),
-                \ 'style': 'minimal'
-                \ }
-        ]]
+        vim.g.rnvimr_layout = {
+            relative = 'editor',
+            width = math.floor(0.75 * vim.o.columns), -- 90% of screen width
+            height = math.floor(0.75 * vim.o.lines),  -- 90% of screen height
+            col = math.floor(0.15 * vim.o.columns),  -- 5% offset to center horizontally
+            row = math.floor(0.15 * vim.o.lines),    -- 5% offset to center vertically
+            style = 'minimal'
+        }
     end,
+
     keys = {
-        {'<leader>r', function() vim.cmd 'RnvimrToggle' end, silent = true, desc = 'Ranger'}
+        {'<leader>r', function() vim.cmd 'RnvimrToggle' end, silent = true, desc = 'Ranger'},
+        { "<C-f>", function() require('user.libs.snacks-dirs').find_dirs(function(path) vim.cmd('e ' .. path) end) end, desc = "Find directories (ranger)" },
+        {'<leader>R', function() vim.cmd 'e .' end, desc = 'Ranger Root'}
     }
 }
