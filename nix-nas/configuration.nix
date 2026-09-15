a200b3b105ffab1e0522c0c988079dc68779855d
Oleksii Panchenko <alex.pan4@proton.me>
2026-09-15 20:53:58 +0100

    [nas] no sleep!

--- nix-nas/configuration.nix
+++ nix-nas/configuration.nix
@@ -188,6 +188,12 @@
 
   environment.shells = [ pkgs.fish ];
 
+# Disable systemd sleep, suspend, and hibernation targets
+  systemd.targets.sleep.enable = false;
+  systemd.targets.suspend.enable = false;
+  systemd.targets.hibernate.enable = false;
+  systemd.targets.hybrid-sleep.enable = false;
+
   # List packages installed in system profile.
   # You can use https://search.nixos.org/ to find more packages (and options).
   # environment.systemPackages = with pkgs; [
